b5499f42147a9898cf4d4fdebc1f58f5d1b3d4f2
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-01-09 20:06:27 +0900

    조건부 렌더링

--- src/Hello.js
+++ src/Hello.js
@@ -3,7 +3,16 @@ import React from 'react';
 // 함수형으로 리액트 컴포넌트 만들어보기
 function Hello(props) {
   const style = { color: props.color };
-  return <div style={style}>안녕하세요 {props.name}</div>;
+
+  // {null}, {undefined}, {false}는 아무것도 렌더링 되지 않는다 ({0}은 됨!)
+  // 조건부 렌더링 - &&연산자를 사용하거나 3항 연산자를 사용한다.
+  return (
+    <div style={style}>
+      {props.isSpecial && <b>*</b>}
+      {/* {props.isSpecial ? <b>*</b> : null} */}
+      안녕하세요 {props.name}
+    </div>
+  );
 }
 
 // props가 없을 때 default props 설정 방법
