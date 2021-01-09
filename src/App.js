d304b02a143892c7b52f9b5c0e0748b685c92ca8
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-01-09 19:54:06 +0900

    props를 통해 컴포넌트에게 값 전달하기

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import Hello from './Hello.js';
-import './App.css';
+import Wrapper from './Wrapper.js';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem',
-  };
-
   return (
-    <>
-      {/* this is comment */}
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Wrapper>
+      <Hello name="react" color="red" />
+      <Hello color="pink" />
+    </Wrapper>
   );
 }
 
 export default App;
-
-/* JSX 규칙
-1. 태그는 꼭 닫아주어야 한다.
-2. self-closing tag를 꼭 사용해주어야한다. ex) <br />
-3. 2개 이상의 태그는 감싸주어야한다. or fragment를 사용한다. <>[tags]</> (이러면 html로 변환될 때 불필요한 태그가 생기지 않는다.)
-4. 자바스크립트 변수/상수를 사용하려면 {} 중괄호로 감싸준다.
-5. style 객체를 만들땐 dash 를 camel case로 변환하영 사용해준다.
-6. class는 className을 사용한다.
-7. 주석은 { 주석 } 중괄호로 감싸준다.
-*/
--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 // 함수형으로 리액트 컴포넌트 만들어보기
-function Hello() {
-  return <div>안녕하세요</div>;
+function Hello(props) {
+  const style = { color: props.color };
+  return <div style={style}>안녕하세요 {props.name}</div>;
 }
+
+// props가 없을 때 default props 설정 방법
+// default가 없다면 undefined 반환
+Hello.defaultProps = {
+  name: '이름없음',
+};
 
 export default Hello;
