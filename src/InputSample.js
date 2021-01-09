8b16ae71af447cc42ceb8fb23b887a75b197fa5e
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-01-09 23:47:56 +0900

    여러개의 input 상태 관리하기
    여러 input을 객체로 묶어서 관리
    jsx 에서 input의 name 프로퍼티를 키로 활용해서 각각의 input들의 value를 관리하는 것이 핵심!
    객체, 배열 복사 시 spread 문법을 활용하자
    
    새로 배운 문법: computed property names (es2015)

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,21 +1,47 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-  const [text, setText] = useState(' ');
+  const [inputs, setInputs] = useState({
+    name: ' ',
+    nickname: ' ',
+  });
+
+  const { name, nickname } = inputs;
+
   const onChange = (e) => {
-    setText(e.target.value);
+    const { name, value } = e.target;
+
+    // 이렇게 간단하게 표현할 수 있다.
+    setInputs({
+      // 객체나 배열에서 꼭 spread 문법을 사용해서 불변성을 지켜주어야 한다.
+      // 불변성을 지켜주어야 react component에서 상태가 업데이트 됨을 감지하고 그에 따른 렌더링을 해준다.
+      ...inputs,
+      [name]: value, // ES2015 computed property names
+    });
+
+    // const newInputs = {
+    //   ...inputs,
+    // };
+
+    // newInputs[name] = value;
+    // setInputs(newInputs);
   };
+
   const onReset = () => {
-    setText(' ');
+    setInputs({
+      name: ' ',
+      nickname: ' ',
+    });
   };
 
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
       <button onClick={onReset}>초기화</button>
       <div>
         <b>값: </b>
-        {text}
+        {name} ({nickname})
       </div>
     </div>
   );
