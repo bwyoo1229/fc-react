06977c0722177b124d638873b6f45b87f7af63b6
Byeonguk Yoo <bwyoo1229@gmail.com>
2021-01-11 19:21:23 +0900

    배열에 항목 추가하기
    Basic Hooks (API)
    const [state, setState] = useState(initialState);
    - returns a stateful value, and a function to update it.
    - setState function is used to update the state. It accepts a new state value and enqueues a re-render of the component.
    
    리액트에서의 리렌덩링은 state 와 props의 변화를 감지해서 변화가 일어날 경우 리렌더링함.
    하지만 불변성을 유지하지 않으면 리렌더링 일어나지 않음.
    
    ex) array = newArray;
    		newArray.push('new');
    		array !== newArray // false  --> 리렌더링 일어나지 않음

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
+import CreateUser from './CreateUser';
 import UserList from './UserList';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  // 배열을 컴포넌트의 상태로써 관리하는 법
+  // useState로 감싸주면 된다.
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -18,17 +36,30 @@ function App() {
       username: 'liz',
       email: 'liz@example.com',
     },
-  ];
+  ]);
 
   // useRef로 값은 변화해도 component가 re-rendering 되는 것을 막을 수 있다.
   const nextId = useRef(4);
 
   const onCreate = () => {
-    console.log(nextId.current);
+    const user = {
+      id: nextId.current,
+      ...inputs,
+    };
+    setUsers([...users, user]);
+    setInputs({
+      username: '',
+      email: '',
+    });
     nextId.current += 1;
   };
 
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
